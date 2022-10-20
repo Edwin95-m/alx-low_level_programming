d203b762824d82533856aee0aab289f3c8bd5a3e
edwin <edwinmariti95@gmail.com>
2022-10-20 07:47:28 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: print alphabet
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}

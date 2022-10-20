ea7dc95ca5557a150eb3d8389ba5e7208625c7c3
edwin <edwinmariti95@gmail.com>
2022-10-20 05:27:20 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /*main function*/
 int main(void)/*main function to return a string*/
 {
-	printf("with proper grammar, but the ourcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }

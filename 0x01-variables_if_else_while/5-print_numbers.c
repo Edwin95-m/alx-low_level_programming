c105b0b9e64e288298c96406ca1f6f24ff6cfb31
edwin <edwinmariti95@gmail.com>
2022-10-20 07:29:32 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry poit
+ * Description: print numbers
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+	return (0);
+}

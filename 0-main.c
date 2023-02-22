0e113a48b9379913bc3ce55cd105173fd1e99f85
edwin <edwinmariti95@gmail.com>
2023-02-22 03:36:57 -0800

    positive or negative integer

---
+++ 0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**main-entry point
+ * description- test for positive negative function
+ * return 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+	i = 98;
+	positive_or_negative(i);
+
+	return(0);
+
+}
